bb6f5e581836ae171ee0127bbad3d44518933193
Precious Kindo <lilonaony@gmail.com>
2017-03-13 21:10:14 +0100

    add filter method

--- filter.py
+++ filter.py
@@ -10,7 +10,7 @@ def main():
         if len(sys.argv) == 2:
             domain = str(sys.argv[1])
             if len(domain) == 22:
-                #todo : filterContent
+                filterContent(domain)
             else:
                 printErrorAndQuit()
         else:
@@ -19,10 +19,16 @@ def main():
         print( str(e) )
         printErrorAndQuit()
 
+def filterContent(url):
+    """ filter .onions """
+    INDEX_NAME = "crawling"
+    url = "http://localhost:9200/" + INDEX_NAME # URL of the new index
+    print('\033[1;30m Test that Elasticsearch is available: %s \033[1;m' % url)
+
 def printErrorAndQuit():
     """Printing the usage information"""
     print("Filters an onion domain from the index.\n")
-    print("Usage: ./filter.py some.onion")
+    print("Usage: ./filter.py somesite0128dj2.onion")
     print("Example: ./filter.py msydqstlz2kzerdg.onion\n")
     sys.exit()
 
